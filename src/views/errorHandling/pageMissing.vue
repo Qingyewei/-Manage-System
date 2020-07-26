739bae5e6f1dae6799cfcc491fece40d4857b938
Qingyewei <1396681566@qq.com>
2020-07-26 18:28:11 +0800

    更改信息

--- src/views/errorHandling/pageMissing.vue
+++ src/views/errorHandling/pageMissing.vue
@@ -8,7 +8,7 @@
       <div class="error-desc">啊哦~ 你所访问的页面不存在</div>
       <div class="error-handle">
         <router-link to="/">
-          <el-button type="primary" size="large">返回首页</el-button>
+          <el-button type="primary" size="large">返回登录页</el-button>
         </router-link>
         <el-button class="error-btn" type="primary" size="large" @click="goBack">返回上一页</el-button>
       </div>
